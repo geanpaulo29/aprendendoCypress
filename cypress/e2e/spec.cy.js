042c7593e7abd9e51ad22b22c3f3468e2fbb8761
geanpaulo29 <geanp.antonio@gmail.com>
2025-03-07 12:03:18 -0300

    commit tarefa

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,20 @@
-describe('Teste completo de esperas e assercoes', () => {
-  it('DEvo acessar a página e aguardar os elementos', () => {
-    cy.visit('https://example.cypress.io/todo')
+describe('Formulário de Cadastro', () => {
+  it('Preenche e envia o formulário com sucesso', () => {
+    cy.visit('index.html');
+
+    cy.get('#nome').type('João Silva');
+    cy.get('#email').type('joao.silva@email.com');
+    cy.get('#telefone').type('1199999999');
+    cy.get('#senha').type('Teste@123');
+    cy.get('#confirma_senha').type('Teste@123');
+
+    cy.get('#nome').should('have.value', 'João Silva');
+    cy.get('#email').should('have.value', 'joao.silva@email.com');
+    cy.get('#telefone').should('have.value', '1199999999');
+    cy.get('#senha').should('have.value', 'Teste@123');
+    cy.get('#confirma_senha').should('have.value', 'Teste@123');
+
+    cy.get('form').submit();
+
   });
-})
+});
