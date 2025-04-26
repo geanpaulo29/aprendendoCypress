f5a0dc32e3c6bc9a37525ca10c10175dfdc498bb
geanpaulo29 <geanp.antonio@gmail.com>
2025-04-26 16:19:28 -0300

    commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,46 +1,56 @@
-describe('Teste de Avaliação de Veículo', () => {
-  beforeEach(() => {
-      cy.visit('http://127.0.0.1:5500/index.html');
-  });
+describe('Testes da plataforma EduTech', () => {
 
-  it('Verifica se os elementos estão visíveis', () => {
-      cy.contains('h1', 'Avaliação do Veículo').should('be.visible');
-      cy.get('#nome').should('be.visible');
-      cy.get('#comentario').should('be.visible');
-      cy.get('#submit-button').should('be.visible');
-      cy.get('#avaliacoes').should('be.empty');
-  });
-
-  it('Valida textos e atributos', () => {
-      cy.get('#submit-button').should('have.text', 'Enviar Avaliação');
-      cy.get('#nome').should('have.attr', 'placeholder', 'Digite seu nome');
-      cy.get('#comentario').should('have.attr', 'placeholder', 'Descreva sua experiência com o veículo...');
-  });
-
-  it('Testa envio de avaliação com dados válidos', () => {
-      cy.get('#nome').type('João');
-      cy.get('#comentario').type('Ótimo carro, muito confortável!');
-      cy.get('#submit-button').click();
-
-      cy.get('#loading').should('be.visible');
-      cy.wait(3000);
-      cy.get('#loading').should('not.be.visible');
-      cy.get('#avaliacoes').contains('João: Ótimo carro, muito confortável!');
-  });
-
-  it('Testa erro ao enviar sem nome', () => {
-      cy.get('#comentario').type('Muito econômico!');
-      cy.get('#submit-button').click();
+    beforeEach(() => {
+      cy.clearLocalStorage();
+    });
+  
+    it('Navegar da Home para a página de Login', () => {
+      cy.visit('/home.html');
+      cy.contains('Entrar').click();
+      cy.url().should('include', '/login.html');
+    });
+  
+    it('Realizar login com sucesso', () => {
+      cy.visit('/login.html');
+      cy.get('#email').type('aluno@edutech.com');
+      cy.get('#senha').type('curso123');
+      cy.get('form').submit();
+      cy.url().should('include', '/painel.html');
+      cy.contains('Bem-vindo, aluno!');
+    });
+  
+    it('Mostrar erro em login inválido', () => {
+      cy.visit('/login.html');
+      cy.get('#email').type('aluno@edutech.com');
+      cy.get('#senha').type('senhaErrada');
+      
       cy.on('window:alert', (str) => {
-          expect(str).to.equal('Por favor, preencha todos os campos.');
+        expect(str).to.equal('E-mail ou senha incorretos!');
       });
+  
+      cy.get('form').submit();
+    });
+  
+    it('Redirecionar para login ao acessar página protegida sem estar logado', () => {
+      cy.visit('/meus-cursos.html');
+      cy.url().should('include', '/login.html');
+    });
+  
+    it('Permitir logout e voltar para tela de login', () => {
+      cy.visit('/login.html');
+      cy.get('#email').type('aluno@edutech.com');
+      cy.get('#senha').type('curso123');
+      cy.get('form').submit();
+  
+      cy.url().should('include', '/painel.html');
+  
+      cy.get('#logout').click();
+  
+      cy.url().should('include', '/login.html');
+  
+      cy.visit('/painel.html');
+      cy.url().should('include', '/login.html');
+    });
+  
   });
-
-  it('Testa erro ao enviar sem comentário', () => {
-      cy.get('#nome').type('Maria');
-      cy.get('#submit-button').click();
-      cy.on('window:alert', (str) => {
-          expect(str).to.equal('Por favor, preencha todos os campos.');
-      });
-  });
-});
+  
