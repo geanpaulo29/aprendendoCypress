4751ef03d756500f5f0eeb1c997fca9603ed8c78
geanpaulo29 <geanp.antonio@gmail.com>
2025-05-11 16:00:56 -0300

    commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,56 +1,29 @@
-describe('Testes da plataforma EduTech', () => {
+describe('Login do Aluno - Área de Membros', () => {
 
-    beforeEach(() => {
-      cy.clearLocalStorage();
-    });
-  
-    it('Navegar da Home para a página de Login', () => {
-      cy.visit('/home.html');
-      cy.contains('Entrar').click();
-      cy.url().should('include', '/login.html');
-    });
-  
-    it('Realizar login com sucesso', () => {
-      cy.visit('/login.html');
-      cy.get('#email').type('aluno@edutech.com');
-      cy.get('#senha').type('curso123');
-      cy.get('form').submit();
-      cy.url().should('include', '/painel.html');
-      cy.contains('Bem-vindo, aluno!');
-    });
-  
-    it('Mostrar erro em login inválido', () => {
-      cy.visit('/login.html');
-      cy.get('#email').type('aluno@edutech.com');
-      cy.get('#senha').type('senhaErrada');
-      
-      cy.on('window:alert', (str) => {
-        expect(str).to.equal('E-mail ou senha incorretos!');
-      });
-  
-      cy.get('form').submit();
-    });
-  
-    it('Redirecionar para login ao acessar página protegida sem estar logado', () => {
-      cy.visit('/meus-cursos.html');
-      cy.url().should('include', '/login.html');
-    });
-  
-    it('Permitir logout e voltar para tela de login', () => {
-      cy.visit('/login.html');
-      cy.get('#email').type('aluno@edutech.com');
-      cy.get('#senha').type('curso123');
-      cy.get('form').submit();
-  
-      cy.url().should('include', '/painel.html');
-  
-      cy.get('#logout').click();
-  
-      cy.url().should('include', '/login.html');
-  
-      cy.visit('/painel.html');
-      cy.url().should('include', '/login.html');
-    });
-  
+  beforeEach(() => {
+    cy.visit('http://localhost:5500/index.html'); 
   });
-  
+
+  it('Fazer login corretamente, criar cookie e salvar no localStorage', () => {
+    cy.get('#email').type('aluno@curso.com');
+    cy.get('#senha').type('curso2024');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('#mensagem').should('contain', 'Acesso concedido!');
+
+    cy.getCookie('curso_token').should('exist').and('have.property', 'value', 'curso123');
+
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem('aluno_email')).to.equal('aluno@curso.com');
+    });
+
+    cy.reload();
+
+    cy.getCookie('curso_token').should('exist').and('have.property', 'value', 'curso123');
+
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem('aluno_email')).to.equal('aluno@curso.com');
+    });
+  });
+
+});
